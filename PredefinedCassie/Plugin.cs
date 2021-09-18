a26f7795d5fd88e208b89ad080266a83c3c2f3bd
Ceglaa <84150438+Ceglaa@users.noreply.github.com>
2021-09-18 11:18:40 +0200

    Add files via upload

--- PredefinedCassie/Plugin.cs
+++ PredefinedCassie/Plugin.cs
@@ -11,7 +11,7 @@
         public override string Name { get; } = "PredefinedCassie";
         public override string Prefix { get; } = "PredefinedCassie";
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
-        public override Version Version { get; } = new Version(1, 0, 3);
+        public override Version Version { get; } = new Version(1, 0, 4);
 
         public override void OnEnabled()
         {
